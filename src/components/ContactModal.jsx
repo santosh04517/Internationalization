af259b8fafab00049a288730d65a0e997e2a4fea
Arunabha-NOTE <arunabham2017@gmail.com>
2025-04-15 00:26:43 +0530

    added about and some changes just changing links  to internationalization is left for prismic

--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -5,18 +5,19 @@ const ContactModal = () => {
     return (
       <div>
         {/* Open the modal using document.getElementById('ID').showModal() method */}
-        <button className="btn btn-primary" onClick={()=>document.getElementById('my_modal_1').showModal()}>Consult us</button>
+        <button className="btn btn-primary" onClick={()=>document.getElementById('my_modal_1').showModal()}>Socials</button>
         <dialog id="my_modal_1" className="modal">
-          <div className="modal-box">
-            <h3 className="font-bold text-lg">Consult Here</h3>
-            <p className="py-4">Press ESC key or click the button below to close</p>
-            <div className="modal-action">
-              <form method="dialog">
-                {/* if there is a button in form, it will close the modal */}
-                <button className="btn">Close</button>
-              </form>
+            <div className="modal-box">
+                <p className="pt-4 pb-1">Linkedin: <a href="https://www.linkedin.com/in/dr-santosh-patil-28b92650/" target="_blank">https://www.linkedin.com/in/dr-santosh-patil-28b92650/</a></p>
+                <p className="py-1">Google Scholar: <a href="https://scholar.google.com/citations?user=5g1MIjAAAAAJ&hl=en" target="_blank">https://scholar.google.com/citations?user=5g1MIjAAAAAJ&hl=en</a></p>
+                <p className="pt-1 pb-4">Email: santosh.patil@jaipur.manipal.edu</p>
+                <div className="modal-action">
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn">Close</button>
+                    </form>
+                </div>
             </div>
-          </div>
         </dialog>
       </div>
     );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,26 +1,34 @@
 import ContactModal from "../components/ContactModal.jsx";
-import Hero from "../components/ContactModal.jsx";
+
 
 const Home = () => {
-  return (
-    <div className="mx-auto max-w-7xl px-2">
-      <div className="hero min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl" />
-            <div>
-            <h1 className="text-5xl font-bold">About Me</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-              quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-            <ContactModal></ContactModal>
-          </div>
+    return (
+        <div className="mx-auto max-w-7xl px-2">
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img
+                        src="http://localhost:5173/img.png"
+                        className="max-w-sm rounded-lg shadow-2xl"/>
+                    <div>
+                        <h1 className="text-5xl font-bold">About</h1>
+                        <p className="py-6">
+                            Dr. Santosh Patil is a Professor in the Department of Mechanical Engineering at Manipal
+                            University Jaipur. He teaches Design, Finite Element Methods, and CAD. His area of research
+                            includes Mechanics, Basalt/carbon laminates, Stress Analysis, Bone fluid flow, and
+                            Internationalization in Higher education. He has published over 40 research papers in
+                            peer-reviewed international journals/chapters, attended various international conferences,
+                            and has one patent. He was the project manager from 2019 to 2024 in the Erasmus+ CBHE EU
+                            Funding of 989,000 euros in the consortium. Other individual cumulative funding raised was
+                            up to 15 Lakhs. He has good experience in MoU planning/processing, collaborations, and
+                            Internationalization. Conducted 15+ workshops and 6 conferences under the ICAMEN series from
+                            2019 to 2022 and, the ICRAFMN series from 2023 to 2025 in India and Abroad.
+                        </p>
+                        <ContactModal></ContactModal>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
